67b6617b5fbe51b6a16f0b5fd3e627e17bc523a0
Gilbert Bui <Gilbert Bui>
2016-04-14 15:00:34 -0700

    fix merge conflicts

--- Assets/Scripts/GameRooms.cs
+++ Assets/Scripts/GameRooms.cs
@@ -41,7 +41,6 @@ public class Room {
 			return "Don't Eat Me";
 		} else if (type == Constants.MINIGAME_RUNNING_RHINO) {
 			return "Running Rhino";
-<<<<<<< 8c30c2a93d0dc2bad55d95abfd5a0e7b598f8bd4
         } else if (type == Constants.MINIGAME_SEA_DIVIDED) {
             return "Sea Divided";
         }  else if (type == Constants.MINIGAME_MULTI_CONVERGENCE) {
