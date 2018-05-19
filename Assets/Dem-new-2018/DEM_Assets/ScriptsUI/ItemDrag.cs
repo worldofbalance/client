3c5f67a25104a1fd8e2dd7091ed58b45d88bb9a8
joleong1 <joleong1@sfsu.edu>
2018-05-19 00:48:10 -0700

    nothing broken

---
+++ Assets/Dem-new-2018/DEM_Assets/ScriptsUI/ItemDrag.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDrag : MonoBehaviour{
+	public bool dragging = false;
+	public bool collision = false;
+	Vector3 position;
+
+	Ray myRay;
+	RaycastHit hit;
+	Species3DFactory Factory;
+	public GameObject objectToInstantiate;
+
+	void Update(){
+
+	}
+
+	public void drop () {
+		if (PlayerStats.Money <= 0) {
+			gameObject.transform.position = position;
+			return;
+		}
+		myRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (myRay, out hit)) {
+			if (Input.GetMouseButtonUp (0)) {
+				Instantiate (objectToInstantiate, hit.point, Quaternion.identity);
+
+				//objectToInstantiate.AddComponent<
+				//objectToInstantiate.AddComponent<PlayerAnimalBehavior> ();
+				//objectToInstantiate.tag = "Defender";
+
+				gameObject.transform.position = position;
+				PlayerStats.Money--;
+			}
+		}
+		return;
+	}
+		
+	public void beginDrag(){
+		position = gameObject.transform.position;
+		dragging = true;
+	}
+
+	public void drag(){
+		transform.position = Input.mousePosition;
+	}
+
+}
